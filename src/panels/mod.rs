085d5a76d75ab0855f44dbecbaa1278cde0b14ac
SQ8 <linus@sq8.dev>
2025-02-22 19:58:47 +0100

    panel get_rel_vel experimental_fix

--- src/panels/mod.rs
+++ src/panels/mod.rs
@@ -34,7 +34,7 @@ impl Panel {
 
     pub fn get_rel_vel(&self, rot: &Quat, vel: &Velocity) -> LinVel {
         let rotated = rot.mul_vec3(self.offset);
-        let rot_vel = vel.angular.0.cross(rotated);
+        let rot_vel = rotated.cross(self.offset);
         LinVel(rot.mul_vec3(vel.linear.0) - rot_vel)
     }
 
