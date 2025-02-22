1cc5fb40d9872241d8154b561bc714a44f9cc2a8
SQ8 <linus@sq8.dev>
2025-02-22 23:33:03 +0100

    fixed long lived logic problem with rotation

--- src/panels/mod.rs
+++ src/panels/mod.rs
@@ -1,8 +1,8 @@
-use glam::{DQuat as Quat, DVec3 as Vec3};
+use glam::{DQuat as Quat, DVec3 as Vec3, Vec3Swizzles};
 
 use crate::{
     moments::{Force, Moment},
-    velocity::{LinVel, Velocity},
+    velocity::{AngVel, LinVel, Velocity},
     State,
 };
 
@@ -32,10 +32,19 @@ impl Panel {
         Force::new(DENSITY * -v_sq * HALF_C_D * area)
     }
 
+    pub fn rotated(&self, rot: &Quat) -> Self {
+        let offset = rot.inverse().mul_vec3(self.offset);
+        let normal = rot.inverse().mul_vec3(self.normal);
+
+        Self::new(offset, normal, self.area)
+    }
+
+    pub fn tip_velocity(&self, rot: &Quat, vel: &AngVel) -> LinVel {
+        LinVel(vel.0.xzy().cross(self.rotated(rot).offset.xzy()).xzy())
+    }
+
     pub fn get_rel_vel(&self, rot: &Quat, vel: &Velocity) -> LinVel {
-        let rotated = rot.mul_vec3(self.offset);
-        let rot_vel = rotated.cross(self.offset);
-        LinVel(rot.mul_vec3(vel.linear.0) - rot_vel)
+        LinVel(rot.mul_vec3(vel.linear.0)) + self.tip_velocity(rot, &vel.angular)
     }
 
     pub fn to_moment(&self, state: &State) -> Moment {
@@ -44,8 +53,203 @@ impl Panel {
 
         let force = Force::new(rot.mul_vec3(self.to_force(&vel).0));
 
-
         Moment::new(rot.mul_vec3(self.offset), force.0)
+    }
+}
+
+#[cfg(test)]
+mod test_utils {
+    use super::*;
+    use std::f64::consts::PI;
+
+    pub fn quarter_rotations() -> (Quat, Quat, Quat) {
+        (
+            Quat::from_rotation_x(PI / 2.),
+            Quat::from_rotation_y(PI / 2.),
+            Quat::from_rotation_z(PI / 2.),
+        )
+    }
+
+    pub fn xyz_panels() -> (Panel, Panel, Panel) {
+        (
+            Panel::new(Vec3::X, Vec3::X, 1.),
+            Panel::new(Vec3::Y, Vec3::Y, 1.),
+            Panel::new(Vec3::Z, Vec3::Z, 1.),
+        )
+    }
+
+    pub fn xyz_neg_panels() -> (Panel, Panel, Panel) {
+        (
+            Panel::new(-Vec3::X, Vec3::X, 1.),
+            Panel::new(-Vec3::Y, Vec3::Y, 1.),
+            Panel::new(-Vec3::Z, Vec3::Z, 1.),
+        )
+    }
+
+    pub fn xyz_angvel() -> (AngVel, AngVel, AngVel) {
+        (
+            AngVel::with_x(1.0),
+            AngVel::with_y(1.0),
+            AngVel::with_z(1.0),
+        )
+    }
+
+    pub fn xyz_linvel() -> (LinVel, LinVel, LinVel) {
+        (
+            LinVel::with_x(1.0),
+            LinVel::with_y(1.0),
+            LinVel::with_z(1.0),
+        )
+    }
+}
+
+#[cfg(test)]
+mod rotated {
+    use approx::assert_ulps_eq;
+    use test_utils::*;
+
+    use super::*;
+
+    #[test]
+    fn no_rotation() {
+        let p1 = Panel::new(Vec3::X, Vec3::Y, 1.);
+        let q1 = Quat::IDENTITY;
+
+        assert_ulps_eq!(p1.rotated(&q1).offset, p1.offset);
+    }
+
+    #[test]
+    fn quarter_turn_offset() {
+        let (rx, ry, rz) = quarter_rotations();
+        let (px, py, pz) = xyz_panels();
+
+        assert_ulps_eq!(px.rotated(&rx).offset, px.offset);
+        assert_ulps_eq!(px.rotated(&ry).offset, pz.offset);
+        assert_ulps_eq!(px.rotated(&rz).offset, -py.offset);
+
+        assert_ulps_eq!(py.rotated(&rx).offset, -pz.offset);
+        assert_ulps_eq!(py.rotated(&ry).offset, py.offset);
+        assert_ulps_eq!(py.rotated(&rz).offset, px.offset);
+
+        assert_ulps_eq!(pz.rotated(&rx).offset, py.offset);
+        assert_ulps_eq!(pz.rotated(&ry).offset, -px.offset);
+        assert_ulps_eq!(pz.rotated(&rz).offset, pz.offset);
+    }
+
+    #[test]
+    fn quarter_turn_normal() {
+        let (rx, ry, rz) = quarter_rotations();
+        let (px, py, pz) = xyz_panels();
+
+        assert_ulps_eq!(px.rotated(&rx).normal, px.normal);
+        assert_ulps_eq!(px.rotated(&ry).normal, pz.normal);
+        assert_ulps_eq!(px.rotated(&rz).normal, -py.normal);
+
+        assert_ulps_eq!(py.rotated(&rx).normal, -pz.normal);
+        assert_ulps_eq!(py.rotated(&ry).normal, py.normal);
+        assert_ulps_eq!(py.rotated(&rz).normal, px.normal);
+
+        assert_ulps_eq!(pz.rotated(&rx).normal, py.normal);
+        assert_ulps_eq!(pz.rotated(&ry).normal, -px.normal);
+        assert_ulps_eq!(pz.rotated(&rz).normal, pz.normal);
+    }
+}
+
+#[cfg(test)]
+mod tip_velocity {
+    use super::*;
+    use approx::assert_ulps_eq;
+    use test_utils::*;
+
+    #[test]
+    fn stationary() {
+        let p1 = Panel::new(Vec3::X, Vec3::X, 1.);
+        let rot = Quat::IDENTITY;
+        let vel = AngVel::ZERO;
+
+        assert_ulps_eq!(p1.tip_velocity(&rot, &vel), LinVel::ZERO);
+    }
+
+    #[test]
+    fn inline() {
+        let q1 = Quat::IDENTITY;
+
+        let (px, py, pz) = xyz_panels();
+        let (vx, vy, vz) = xyz_angvel();
+
+        assert_ulps_eq!(px.tip_velocity(&q1, &vx), LinVel::ZERO);
+        assert_ulps_eq!(py.tip_velocity(&q1, &vy), LinVel::ZERO);
+        assert_ulps_eq!(pz.tip_velocity(&q1, &vz), LinVel::ZERO);
+    }
+
+    #[test]
+    fn perpendicular() {
+        let q1 = Quat::IDENTITY;
+
+        let (px, py, pz) = xyz_panels();
+        let (ax, ay, az) = xyz_angvel();
+        let (vx, vy, vz) = xyz_linvel();
+
+        assert_ulps_eq!(px.tip_velocity(&q1, &ay), vz);
+        assert_ulps_eq!(py.tip_velocity(&q1, &az), vx);
+        assert_ulps_eq!(pz.tip_velocity(&q1, &ax), vy);
+    }
+
+    #[test]
+    fn perpendicular_reverse() {
+        let q1 = Quat::IDENTITY;
+
+        let (px, py, pz) = xyz_neg_panels();
+        let (ax, ay, az) = xyz_angvel();
+        let (vx, vy, vz) = xyz_linvel();
+
+        assert_ulps_eq!(px.tip_velocity(&q1, &ay), -vz);
+        assert_ulps_eq!(py.tip_velocity(&q1, &az), -vx);
+        assert_ulps_eq!(pz.tip_velocity(&q1, &ax), -vy);
+    }
+
+    #[test]
+    fn perpendicular_backward() {
+        let q1 = Quat::IDENTITY;
+
+        let (px, py, pz) = xyz_panels();
+        let (ax, ay, az) = xyz_angvel();
+        let (vx, vy, vz) = xyz_linvel();
+
+        assert_ulps_eq!(px.tip_velocity(&q1, &-ay), -vz);
+        assert_ulps_eq!(py.tip_velocity(&q1, &-az), -vx);
+        assert_ulps_eq!(pz.tip_velocity(&q1, &-ax), -vy);
+    }
+
+    #[test]
+    fn perpendicular_backward_reverse() {
+        let q1 = Quat::IDENTITY;
+
+        let (px, py, pz) = xyz_neg_panels();
+        let (ax, ay, az) = xyz_angvel();
+        let (vx, vy, vz) = xyz_linvel();
+
+        assert_ulps_eq!(px.tip_velocity(&q1, &-ay), vz);
+        assert_ulps_eq!(py.tip_velocity(&q1, &-az), vx);
+        assert_ulps_eq!(pz.tip_velocity(&q1, &-ax), vy);
+    }
+
+    #[cfg(test)]
+    mod rotated {
+        use super::*;
+
+        #[test]
+        #[allow(unused)]
+        fn inline() {
+            let (qx, qy, qz) = quarter_rotations();
+            let (px, py, pz) = xyz_panels();
+            let (ax, ay, az) = xyz_angvel();
+            let (vx, vy, vz) = xyz_linvel();
+
+            assert_ulps_eq!(px.tip_velocity(&qy, &ax), vy);
+            assert_ulps_eq!(py.tip_velocity(&qz, &az), -vy);
+            assert_ulps_eq!(px.tip_velocity(&qy, &az), LinVel::ZERO);
+        }
     }
 }
 
@@ -66,7 +270,7 @@ mod relative_velocity {
     }
 
     #[test]
-    fn rotated() {
+    fn rotated_stationary() {
         let panel = Panel::new(Vec3::X, Vec3::Y, 1.0);
         let rot = Quat::from_rotation_x(PI / 2.);
         let vel = Velocity::ZERO;
@@ -98,7 +302,7 @@ mod relative_velocity {
         let rot = Quat::IDENTITY;
         let vel = Velocity::new(LinVel::ZERO, AngVel::with_z(1.));
 
-        assert_ulps_eq!(panel.get_rel_vel(&rot, &vel), LinVel::with_y(1.0));
+        assert_ulps_eq!(panel.get_rel_vel(&rot, &vel), LinVel::with_y(-1.0));
     }
 }
 
@@ -173,7 +377,9 @@ mod to_force {
 mod to_moment {
     use super::*;
     use crate::{
-        inertia_mass::{Inertia, InnertiaMass, Mass}, moments::Torque, momentum::{AngMom, LinMom, Momentum}, transform::Transform
+        inertia_mass::{Inertia, InnertiaMass, Mass},
+        momentum::Momentum,
+        transform::Transform,
     };
     use approx::assert_ulps_eq;
 
@@ -227,21 +433,21 @@ mod to_moment {
         let pyz = Panel::new(Vec3::Y, Vec3::Z, 1.);
         let pzx = Panel::new(Vec3::Z, Vec3::X, 1.);
 
-        // momentum /2 bcs inertia is a bit weird
+        // momentum /2 because inertia is a bit weird
         let sx = State::new(mass_x(), Transform::ZERO, Momentum::from_ang(Vec3::X / 2.));
         let sy = State::new(mass_y(), Transform::ZERO, Momentum::from_ang(Vec3::Y / 2.));
         let sz = State::new(mass_z(), Transform::ZERO, Momentum::from_ang(Vec3::Z / 2.));
 
         assert_ulps_eq!(pxy.to_moment(&sx).magnitude(), 0.);
         assert_ulps_eq!(pxy.to_moment(&sy).magnitude(), 0.);
-        assert_ulps_eq!(pxy.to_moment(&sz), Moment::new(Vec3::X, -Vec3::Y * EXP));
+        assert_ulps_eq!(pxy.to_moment(&sz), Moment::new(Vec3::X, Vec3::Y * EXP));
 
-        assert_ulps_eq!(pyz.to_moment(&sx), Moment::new(Vec3::Y, -Vec3::Z * EXP));
+        assert_ulps_eq!(pyz.to_moment(&sx), Moment::new(Vec3::Y, Vec3::Z * EXP));
         assert_ulps_eq!(pyz.to_moment(&sy).magnitude(), 0.);
         assert_ulps_eq!(pyz.to_moment(&sz).magnitude(), 0.);
 
         assert_ulps_eq!(pzx.to_moment(&sx).magnitude(), 0.);
-        assert_ulps_eq!(pzx.to_moment(&sy), Moment::new(Vec3::Z, -Vec3::X * EXP));
+        assert_ulps_eq!(pzx.to_moment(&sy), Moment::new(Vec3::Z, Vec3::X * EXP));
         assert_ulps_eq!(pzx.to_moment(&sz).magnitude(), 0.);
     }
 }
