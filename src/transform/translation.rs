57f233d7dc470f51c19e363173e084b0556a1572
SQ8 <linus@sq8.dev>
2025-03-12 20:35:57 +0100

    chore(tests): translation tests for From<T> trait

--- src/transform/translation.rs
+++ src/transform/translation.rs
@@ -7,7 +7,7 @@ use std::ops;
 /// Represents a linear transformation in 3d space
 ///
 /// The translation is being represented in meters
-#[derive(Debug, Clone, Copy, PartialEq, Approx)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Approx)]
 pub struct Translation(pub Vec3);
 
 impl Translation {
@@ -154,6 +154,21 @@ mod constructors {
 }
 
 #[cfg(test)]
+mod modify {
+    use super::*;
+    use approx::assert_ulps_eq;
+
+    #[test]
+    fn with() {
+        let t = Translation::ZERO;
+
+        assert_ulps_eq!(t.with_x(1.).0, Vec3::X);
+        assert_ulps_eq!(t.with_y(1.).0, Vec3::Y);
+        assert_ulps_eq!(t.with_z(1.).0, Vec3::Z);
+    }
+}
+
+#[cfg(test)]
 mod arithmetic {
     use super::*;
     use approx::assert_ulps_eq;
@@ -217,21 +232,34 @@ mod assignment_arithmetic {
         assert_ulps_eq!(a.0, Vec3::new(2.0, -4.0, 6.0));
     }
 }
+
 #[cfg(test)]
-mod equality {
+mod traits {
     use super::*;
-    use approx::{assert_abs_diff_eq, assert_relative_eq, assert_ulps_eq};
+    use approx::assert_ulps_eq;
 
     #[test]
-    fn test_approx_eq() {
-        let a = Translation::from_x(0.1) + Translation::from_x(0.2);
-        let b = Translation::from_x(0.3);
+    fn vec3_to_trans() {
+        let v1 = Vec3::new(67.95, 96.69, 90.22);
+        let v2 = Vec3::new(-88.62, -41.94, 73.85);
 
-        assert_ne!(a, b); // Normal compare should fail this
+        let t2: Translation = v2.into();
 
-        // But using approx should work
-        assert_abs_diff_eq!(a, b, epsilon = 1e-5);
-        assert_relative_eq!(a, b, epsilon = 1e-5);
-        assert_ulps_eq!(a, b);
+        assert_ulps_eq!(Translation::from(v1), Translation::from_vec3(v1));
+        assert_ulps_eq!(t2, Translation::from_vec3(v2));
+    }
+
+    #[test]
+    fn trans_to_vec3() {
+        let v1 = Vec3::new(-83.77, 56.76, 72.10);
+        let v2 = Vec3::new(41.03,-98.07,-66.08);
+
+        let t1 = Translation::from_vec3(v1);
+        let t2 = Translation::from_vec3(v2);
+
+        let v3: Vec3 = t2.into();
+
+        assert_ulps_eq!(Vec3::from(t1), v1);
+        assert_ulps_eq!(v3, v2);
     }
 }
